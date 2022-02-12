c3bae491329d9f6ed54e09ddf5a9c017f75b29ea
apineapple <apineapple@users.noreply.github.com>
2022-02-12 10:21:32 -0500

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -2,10 +2,13 @@ function initialize()
 {
 $(document).ready(function(){
          $("#projectlist").hide();
+         $("#songlist").hide();
          $(".modal-description").hide(); //neccisary?
          $("#allpatch").hide();
          
-         $("#projects").click(function(){
+          $("#projects").click(function(){
+            $("#songlist").hide();
+            $("#songs").css("color", "black");
           if ( $("#projectlist").is(':visible') ){
         $("#logo").show();
          $("#projectlist").hide();
@@ -17,6 +20,21 @@ $(document).ready(function(){
         $("#projects").css("color", "white");
       }
          });
+
+         $("#songs").click(function(){
+            $("#projectlist").hide();
+            $("#projects").css("color", "black");
+          if ( $("#songlist").is(':visible') ){
+        $("#logo").show();
+         $("#songlist").hide();
+        $("#songs").css("color", "black");
+      }
+      else{
+           $("#logo").hide();
+         $("#songlist").show();
+        $("#songs").css("color", "white");
+      }
+         });
          
          $(".projectz").click(function(){
          $(".modal-background").show();
@@ -26,9 +44,19 @@ $(document).ready(function(){
          $("#interviewlist").hide();*/
          });
 
+         $(".songz").click(function(){
+         $(".modal-background").show();
+         $("#main").hide();
+         $("#songlist").hide();
+         /*$("#songlist").hide();
+         $("#interviewlist").hide();*/
+         });
+
          $(".modal-background").click(function(){
          $(".modal-background").hide();
-         $("#projectlist").show();
+         $("#logo").show();
+         $("#songs").css("color", "black");
+         $("#projects").css("color", "black");
          }).children().click(function(e) {
          return false;
          });
